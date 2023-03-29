3a24b63d0f7ee1ac8f610adc5df99b98256a4bc4
Daniel <danigutierrez913@gmail.com>
2023-03-28 23:13:25 -0300

    Se trato de agregar local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
 //La idea es que se muestren las marcas para que el usuario seleccione
 // una de ellas y que a partir de ese evento se filtre la marca y empiece a mostrar los diferentes modelos para que despues se seleccione y de ahi se lleve el precio para poder cotizar el seguro
-
-
+let segurosCarrito=[];
+const listaProductos = document.querySelector('#lista_productos')
+const contenedorCarrito =document.querySelector('#lista_carrito')
+const carrito = document.querySelector('#carrito')
 let ford = document.querySelector('.marcas__ford')
 let audi = document.querySelector('.marcas__audi')
 let chevrolet = document.querySelector('.marcas__chevrolet') 
 let resultado = document.querySelector('.resultado')
 
+document.addEventListener('DOMContentLoaded',()=>{
+    segurosCarrito=JSON.parse(localStorage.getItem('carrito'))||[];
+    
+})
 
+listaProductos.addEventListener('click',agregarSeguro)
 
 ford.addEventListener('click', () =>{
     console.log("Ingresaste en Ford")
@@ -58,10 +65,10 @@ function mostrarAuto(busqueda){
                 <img src="${auto.img}" width=150px>
                 <p class="card__texto">Valor aproximado ${auto.valor}</p>
                 <p class="card__texto__st">Seguro total ${auto.valor * 0.014 + auto.valor*0.011 + 8000}</p>
-                <button id='${auto.valor}' class="btn__cotizar__total">Agregar seguro total</button>
+                <button id='${auto.valor}' class="btn__cotizar__total agregar-carrito">Agregar seguro total</button>
                 <p class="card__texto__sb">Seguro base ${auto.valor * 0.014 + auto.valor*0.0085 + 8000}</p>
                 <button id='${auto.valor}' 
-                class="btn__cotizar__base">Agregar seguro base</button>
+                class="btn__cotizar__base agregar-carrito">Agregar seguro base</button>
                 
             </div>`;
         resultado.appendChild(cardAuto);
@@ -69,6 +76,24 @@ function mostrarAuto(busqueda){
 
 
 }
+
+function agregarSeguro(evt){
+    evt.preventDefault()
+
+    if(evt.target.classList.contains('agregar-carrito')){
+        const seguro = evt.target.parentElement.parentElement;
+
+        leerDatosSeguro(seguro)
+    }
+}
+
+function leerDatosSeguro(seguro){
+
+    const infoSeguro={
+        imagen: seguro.querySelector('img').src,
+        titulo: seguro.querySelector('h4').textContent,
+    }
+} 
 
 
 
